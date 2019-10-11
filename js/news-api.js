b1422d62322cf364b9d084f1d6260d9d4b9bf924
Pihh <filipemotasa@hotmail.com>
2019-10-11 01:09:15 +0100

    really simple one to understand how it works

---
+++ js/news-api.js
@@ -0,0 +1,5 @@
+const apiKey = "40131404bdb2441dbb9caba18a3973d4";
+const topHeadlinesUrl =
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + apiKey;
+
+export { apiKey, topHeadlinesUrl };
